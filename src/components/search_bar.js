0d0c54bc858ba195c6a3be1b1d77af1ea624f3f4
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-14 12:27:41 -0500

    made onChange more concise

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,7 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
-  }
-
-  onInputChange(evt) {
-    console.log(evt.target.value);
+    return <input onChange={evt => console.log(evt.target.value)} />;
   }
 
 }

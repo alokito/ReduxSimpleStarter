3453bd57faaddb0f3dbcabc3d4018a9da084e6c8
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-14 12:45:23 -0500

    added code to display term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,17 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {term: ''};
+  }
   render() {
-    return <input onChange={evt => console.log(evt.target.value)} />;
+    return <div>
+        <input onChange={
+          evt => this.setState({term: evt.target.value})
+        } >
+        Value of the input:  {this.state.term}
+      </div>;
   }
 
 }

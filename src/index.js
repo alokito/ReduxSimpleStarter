cb05338f5cfb2023630ef696c6cab2df5aea3081
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-15 07:50:24 -0500

    got child working in a hack way

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAY3SjUYswBfQwBeWQkfH9me4QGBKk3K6M';
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
         <div>
           <SearchBar />
+          <VideoDetail video={ this.state.videos[0]} />
           <VideoList videos={ this.state.videos } />
         </div>
     );

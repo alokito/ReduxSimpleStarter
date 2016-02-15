d3f5168b59ce93c61006a860e01838f1741dec6b
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-15 07:28:00 -0500

    added initial search in constructor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyAY3SjUYswBfQwBeWQkfH9me4QGBKk3K6M';
 
 // Create a new component
 // Produces HTML
-const App = () => {
-	return (
-  <div>
-      <SearchBar />
-  </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+        <div>
+          <SearchBar />
+        </div>
+    );
+  }
 }
 
 // take this component's generated HTML and put it on the page

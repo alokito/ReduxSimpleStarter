362941c8569d2d16698a29814917adb2cec41bb7
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-17 09:17:55 -0500

    added input change function

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,10 +4,11 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term: ''}
+    this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(evt) {
-    console.log(evt.target.value);
+    this.setState({term: evt.target.value});
   }
 
   render() {

269adb3fa481740e79f7a1f823291e0354e6ed5d
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-18 10:32:53 -0500

    added componentWillMount method

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => {
-  return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    console.log("should fetch");
+  }
+  render() {
+    return (
+        <div>List of blog Posts</div>
+    );
+  }
+}
+
+export default PostsIndex;

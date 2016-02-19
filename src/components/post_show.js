1d702af931d8c7406e5540d431833c054273459d
Alok Saldanha <alok.saldanha@novartis.com>
2016-02-19 10:36:36 -0500

    added links to posts index

---
+++ src/components/post_show.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export default class {
+  render() {
+    return <div> show {this.props.params.id}</div>;
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import PostsIndex from './components/posts_index';
 import PostCreate from './components/post_create';
+import PostShow from './components/post_show';
 import App from './components/app';
 
 export default (
  <Route path="/" component={App} >
    <IndexRoute component={PostsIndex} />
    <Route path="create" component={PostCreate} />
+   <Route path="posts" component={PostsIndex} />
+   <Route path="posts/:id" component={PostShow} />
  </Route>
 );
 
